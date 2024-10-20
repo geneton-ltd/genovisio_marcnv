d84f1e5ce34c94764467f0cd4ed79f6cf69a76e7
Jozef Sitarcik <jozefsitarcik@gmail.com>
2024-10-20 21:33:49 +0200

    perf: bumped annotation package

--- marcnv/src/acmg/core.py
+++ marcnv/src/acmg/core.py
@@ -8,3 +8,6 @@ ACMG_LOSS_TSV_FILEPATH = os.path.join(SRC_DIR, "data", "acmg_loss.tsv")
 # ACMG criteria constants
 DUPLICATION_GENES_THRESHOLDS = (35, 50)
 DELETION_GENES_THRESHOLDS = (25, 35)
+
+HI_TS_SCORES = [3]
+MIN_FREQUENCY_BENIGN = 0.005
